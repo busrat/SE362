4c49fbc30a974e4f0a96b1d3a276781e9528f324
busrat <tabakbusra@gmail.com>
2018-05-13 19:49:38 +0300

    Update search.php

--- search.php
+++ search.php
@@ -1,31 +1,28 @@
 <?php
-$con = @mysql_connect('localhost', 'root', '', 'project');
+    @mysql_select_db("project", mysql_connect("localhost","root",""));
 
-if (!$con) {
-    echo "Error: " . mysqli_connect_error();
-	exit();
+    $query = $_POST['query'];
+    $topic = mysql_query("select * from courses where code like '%$query%'");
+
+$number=mysql_num_rows($topic); 
+    if($query=="") {
+        echo "You can not make a blank call";
 }
-echo 'Connected to MySQL';
 
-$search=0;
-$search_exploded = explode ( " ", $search ); 
-$x = 0; 
+else if ($number<1) { echo "No results found for your search <a href='index.php'> Turn back.</a>";} 
 
-foreach( $search_exploded as $search_each ) { $x++; $construct = " "; 
-if( $x == 1 ) 
-	$construct .= "keywords LIKE '%$search_each%' "; 
-else 
-	$construct .= "AND keywords LIKE '%$search_each%' "; } 
-$construct = " SELECT * FROM SEARCH_ENGINE WHERE $construct "; 
-$run = mysqli_query( $con,  $construct ); 
-$foundnum = mysql_num_rows($run);
-if ($foundnum == 0) echo "Sorry, there are no matching result for <b> $search </b>. </ br > </ br > 1. Try more general words. for example: If you want to search 'how to create a website' then use general keyword like 'create' 'website' </ br > 2. Try different words with similar meaning </ br > 3. Please check your spelling"; 
-else { echo "$foundnum results found !<p>"; 
-while ( $runrows = mysql_fetch_assoc($run) )
-	{ $title = $runrows ['title']; 
-$desc = $runrows ['description']; 
-$url = $runrows ['url']; 
-echo "<a href='$url'> <b> $title </b> </a> <br> $desc <br> <a href='$url'> $url </a> <p>"; } } 
+else {
+    while($find = mysql_fetch_array($topic)) {
+        $code = $find['code'];
+        $name = $find['name'];
+        $link = $find['link'];
 
+    echo $code." ".$name." ";
+    echo '<a href="' . $link . '">Go to page of the course</a>';
 
+    }
+}
 ?>
+
+<a href="comment.php"><br /><br /><br />Click here to see comments about the course</a>
+   
