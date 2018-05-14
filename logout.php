9f037ebfc22cd63d5eaa142206fce9e90f34f61d
sevvalp <38830988+sevvalp@users.noreply.github.com>
2018-05-14 22:47:31 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,21 +1,8 @@
-
-<?php
-session_start();
-if(session_destroy())
-{
-header("Location: login.php");
-}
-
-/* session_start();
-session_unset();
-session_destroy();
-ob_start();
-header("location:login.php"); 
-//burası da home olacak ilerde
-ob_end_flush(); 
-include 'login.php';
-//include 'home.php';
-exit(); */
-
+<?php 
+include('server.php');
+                if(isset($_SESSION['name']))
+                {
+                unset($_SESSION['name']);
+                }
+                echo '<h1>You have been successfully logged out</h1>';
 ?>
-
