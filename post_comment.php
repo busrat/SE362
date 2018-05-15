983c8389be1f27242b653566cf68b4bafc0a1da5
sevvalp <38830988+sevvalp@users.noreply.github.com>
2018-05-15 13:44:53 +0300

    Update post_comment.php

--- post_comment.php
+++ post_comment.php
@@ -1,15 +1,17 @@
 <?php
+include('server.php');
+include('search.php');
 @mysql_select_db("project", mysql_connect("localhost","root",""));
-$username = $_POST["username"];
-$comment = $_POST["comment"];
+
+$username= $_SESSION['username'];
+$comment = $_POST['comment'];
 $comment_length = strlen($comment);
-if($comment_length > 100) {
+if($comment_length > 250) {
 	header("location: comment.php?error=1");
 }
 else
 {
-		mysql_query("INSERT INTO comments VALUES('$code','$comment','$username')");
+		mysql_query("INSERT INTO comments VALUES('$comment','$username')");
         header("location: comment.php");
 }
-
 ?>
