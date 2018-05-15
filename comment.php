8ede09546ab1822af4e808673b607437b727b852
sevvalp <38830988+sevvalp@users.noreply.github.com>
2018-05-15 19:25:19 +0300

    Update comment.php

--- comment.php
+++ comment.php
@@ -1,3 +1,15 @@
+<head>
+	 <link rel="stylesheet" type="text/css" href="style.css">
+	 <?php 
+	 include('server.php');
+	 include_once("menu.php"); 
+	 if(empty($_SESSION['username'])){
+		 echo "You cannot comment on the courses without logging in.";
+exit;
+} ?>
+</head>
+
+
 <html>
 <form action="post_comment.php" method="POST"> 
     <input type="text" name="code" value="Code of the course" onfocus="this.value==defaultValue?this.value='':null"><br>
@@ -6,3 +18,4 @@
 
 </form>
 </html>
+
