d4aa8a75b392c69da2c19e0da9950413eaacb6fd
sevvalp <38830988+sevvalp@users.noreply.github.com>
2018-05-15 13:45:23 +0300

    Update comment.php

--- comment.php
+++ comment.php
@@ -3,20 +3,18 @@
 </html>
 
 <?php
+session_start();
 @mysql_select_db("project", mysql_connect("localhost","root",""));
-
 $find_data = mysql_query("SELECT * FROM comments");
 while($row = @mysql_fetch_assoc($find_data)) {
-	$code = $row['code'];
-	$username = $row['username'];
+	$username= $row['username'];
 	$comment = $row['comment'];
 	echo "$comment - $username<p>";
 }
 ?>
 
 <html>
-<form action="post_comment.php" method="POST">
-	  <input type="text" name="username" value="User Name"><br> 
+<form action="post_comment.php" method="POST"> 
 	<textarea name="comment" cols="50" rows="2">Enter a comment</textarea>
 	<input type="submit" value="Comment">
 
