a3812d30837a76f34d6c1d0cb070a3ec396954db
busrat <tabakbusra@gmail.com>
2018-05-15 17:46:29 +0300

    Update post_comment.php

--- post_comment.php
+++ post_comment.php
@@ -1,6 +1,7 @@
 <?php
 include('server.php');
-include('search.php');
+
+include('comment.php');
 @mysql_select_db("project", mysql_connect("localhost","root",""));
 $username= $_SESSION['username'];
 $comment = $_POST['comment'];
@@ -9,8 +10,8 @@ $comment_length = strlen($comment);
 if($comment_length > 250) {
 	header("location: comment.php?error=1");
 }
-else 
-{
+else if($code=="" || $comment=="") { echo "Please fill al the fields."; }
+else{
 		mysql_query("INSERT INTO comments VALUES('$comment','$username','$code')");
         header("location: comment.php");
 }
