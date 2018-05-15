734bf67c945710fcf7825011cab1e44ccbe7b33b
busrat <tabakbusra@gmail.com>
2018-05-15 16:25:30 +0300

    Update post_comment.php

--- post_comment.php
+++ post_comment.php
@@ -2,16 +2,16 @@
 include('server.php');
 include('search.php');
 @mysql_select_db("project", mysql_connect("localhost","root",""));
-
 $username= $_SESSION['username'];
 $comment = $_POST['comment'];
+$code = $_POST['code'];
 $comment_length = strlen($comment);
 if($comment_length > 250) {
 	header("location: comment.php?error=1");
 }
-else
+else 
 {
-		mysql_query("INSERT INTO comments VALUES('$comment','$username')");
+		mysql_query("INSERT INTO comments VALUES('$comment','$username','$code')");
         header("location: comment.php");
 }
 ?>
