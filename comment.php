3500ed7a9b4ca96d30f37f03f86ab0a780ce5da0
busrat <tabakbusra@gmail.com>
2018-05-15 17:47:04 +0300

    Update comment.php

--- comment.php
+++ comment.php
@@ -1,25 +1,8 @@
 <html>
-<h1>Comments</h1>
-</html>
-
-<?php
-session_start();
-@mysql_select_db("project", mysql_connect("localhost","root",""));
-$find_data = mysql_query("SELECT * FROM comments");
-
-
-while($row = @mysql_fetch_assoc($find_data)) {
-	$username= $row['username'];
-	$comment = $row['comment'];
-	echo "$comment - $username<p>";
-}
-?>
-
-<html>
 <form action="post_comment.php" method="POST"> 
-	<input type="text" name="code" value="Code of the course"><br>
-	<textarea name="comment" cols="50" rows="2">Enter a comment</textarea>
-	<input type="submit" value="Comment">
+    <input type="text" name="code" value="Code of the course" onfocus="this.value==defaultValue?this.value='':null"><br>
+    <textarea name="comment" cols="50" rows="2" onfocus="this.value==defaultValue?this.value='':null">Enter a comment</textarea>
+    <input type="submit" value="Comment">
 
 </form>
 </html>
