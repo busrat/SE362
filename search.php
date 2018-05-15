651e0e8e1afeb852e949b3b20d489e5f97b1047a
sevvalp <38830988+sevvalp@users.noreply.github.com>
2018-05-15 22:25:10 +0300

    Update search.php

--- search.php
+++ search.php
@@ -1,5 +1,10 @@
+<ul>
+  <li><a href="index.php">Home sweet home</a></li>
+  <li><a href="index.php">Do you want to search for courses?</a></li>
+  <li><a href="comment.php">Wanna comment on the courses?</a></li>
+</ul>
+
 <?php
-include_once("menu.php"); 
     @mysql_select_db("project", mysql_connect("localhost","root",""));
     $query = $_POST['query'];
     $topic = mysql_query("select * from courses where code like '%$query%'");
